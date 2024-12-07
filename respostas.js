8c4f7e2822042b519d6d287e19f0730ffd4cb2c8
bellinhajoaninha <00001106685854sp@al.educacao.sp.gov.br>
2024-12-07 00:10:16 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,479 @@
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome completo de Harry Potter?',
+    'Harry James Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem são os pais de Harry Potter?',
+    'Lily Potter e James Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem são os melhores amigos de Harry Potter?',
+    'Hermione Granger e Ron Weasley'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual casa de Hogwarts Harry Potter pertence?',
+    'Grifinória'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da escola onde Harry Potter estuda?',
+    'Hogwarts'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o diretor de Hogwarts durante a maior parte da série?',
+    'Alvo Dumbledore'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o primeiro professor de D.C.A.T no filme?',
+    'Quirrell'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é o "Feitiço do Patrono" e quem ensina Harry a conjurá-lo?',
+    'O feitiço do Patrono cria uma criatura protetora, e Harry aprende com Lupin.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o padrinho de Harry Potter?',
+    'Sirius Black'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do vilão principal da série?',
+    'Tom Riddle, também conhecido como Lord Voldemort'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem são os pais de Hermione Granger?',
+    'Sra. Granger e Sr. Granger (ambos são trouxas)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da casa de Hogwarts de Hermione Granger?',
+    'Grifinória'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como Ron Weasley conhece Harry Potter?',
+    'No trem para Hogwarts, no primeiro livro'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o irmão mais velho de Ron Weasley?',
+    'Bill Weasley'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o maior inimigo de Draco Malfoy em Hogwarts?',
+    'Harry Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do banco onde os bruxos guardam seu dinheiro?',
+    'Gringotes'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como chama o feitiço usado para desarmar um oponente?',
+    'Expelliarmus'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o guardião de Hagrid no primeiro livro?',
+    'Fluffy (o cão de três cabeças)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como se chama a vassoura de Harry Potter?',
+    'Nimbus 2000 (nos primeiros livros, depois ele ganha a Firebolt)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quais são as Horcruxes de Voldemort?',
+    'As Horcruxes de Voldemort: diário, anel, medalhão, taça, diadema, Nagini e Harry.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é o "Triwizard Tournament"?',
+    'Um torneio mágico entre três escolas de magia (Hogwarts, Beauxbatons e Durmstrang) com tarefas perigosas.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem ganha o Torneio Tribruxo no quarto livro?',
+    'Harry Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem coloca o nome de Harry no Cálice de Fogo?',
+    'Barty Crouch Jr., disfarçado de Mad-Eye Moody'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quais escolas que participam do Torneio Tribruxo?',
+    'Hogwarts, Beauxbatons e Durmstrang'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do dragão que Harry enfrenta no Torneio Tribruxo?',
+    'O Hungarian Horntail (Dragão Húngaro de Cauda Curva)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é a professora de Poções durante a maior parte da série?',
+    'Severus Snape'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do clube secreto formado por Harry, Hermione e Ron?',
+    'D.A. (Exército de Dumbledore)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o feitiço usado para fazer objetos levitarem?',
+    'Wingardium Leviosa'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do inimigo que Harry enfrenta no segundo livro?',
+    'Tom Riddle (a versão jovem de Voldemort)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como se chama a professora de D.C.A.T no terceiro livro?',
+    'Remus Lupin'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o lobisomem que Harry conhece no terceiro livro?',
+    'Remus Lupin'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é a "Câmara Secreta" em Hogwarts?',
+    'A Câmara Secreta, construída por Slytherin, onde esconde o Basilisco.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como chama o animal de estimação de Harry Potter?',
+    'Hedwig (uma coruja branca)'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o mentor de Harry Potter em "O Prisioneiro de Azkaban"?',
+    'Remus Lupin'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem mata Dumbledore no sexto livro?',
+    'Severus Snape'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é o "Feitiço da Verdade"?',
+    'O feitiço usado para obrigar alguém a dizer a verdade, conhecido como *Vera Verto*.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que ocorre no feitiço de Harry que permite que ele "fale com as cobras"?',
+    'Harry é um "Parsélmago", capaz de falar com cobras devido à conexão com Voldemort.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que são os "Death Eaters"?',
+    'Seguidores de Voldemort, conhecidos como Comensais da Morte.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da mãe de Draco Malfoy?',
+    'Narcisa Malfoy'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é a história por trás do nome "Lord Voldemort"?',
+    'Voldemort vem do francês "vol de mort", que significa "fuga da morte".'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da mãe de Harry Potter?',
+    'Lily Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o diretor da Escola de Magia de Beauxbatons?',
+    'Madame Olympe Maxime'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o diretor da Escola de Magia de Durmstrang?',
+    'Igor Karkaroff'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o professor de Adivinhação de Harry?',
+    'Sibila Trelawney'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como Harry destrói a Horcrux de Tom Riddle no sexto livro?',
+    'Usando a espada de Godric Gryffindor'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da filha de Hermione e Ron?',
+    'Rose Granger-Weasley'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da mãe de Hermione Granger?',
+    'Sra. Granger'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem dá a Harry a espada de Godric Gryffindor?',
+    'Fawkes, a fênix de Dumbledore'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem foi o amigo de infância de Tom Riddle?',
+    'Hokey, a elfa doméstica'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem protege Harry durante o primeiro livro?',
+    'Hagrid'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como Harry consegue falar com cobras?',
+    'Harry é um Parsélmago, devido à sua ligação com Voldemort.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o chefe da casa de Grifinória?',
+    'Professor Minerva McGonagall'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como Harry e seus amigos entram na Câmara Secreta?',
+    'Através de uma entrada secreta em um banheiro.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem foi o primeiro ministro da Magia durante a série?',
+    'Cornelius Fudge'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é a Pedra Filosofal?',
+    'Uma pedra mágica que pode transformar qualquer metal em ouro e criar o Elixir da Vida.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do ônibus mágico em O Prisioneiro de Azkaban??',
+    'O Expresso de Ônibus'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o herói que salva Harry em "O Prisioneiro de Azkaban"?',
+    'Sirius Black'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem se transforma em animago para ajudar Harry?',
+    'Sirius Black, James Potter e Peter Pettigrew.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que são as Relíquias da Morte?',
+    'Três itens mágicos poderosos: a Varinha da Morte, a Pedra da Ressurreição e a Capa da Invisibilidade.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem destrói a Horcrux do medalhão de Slytherin?',
+    'Ron Weasley'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o chefe da casa de Sonserina?',
+    'Professor Severus Snape'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o diretor da escola Durmstrang?',
+    'Igor Karkaroff'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem dá a Harry a dica para encontrar o "Cálice de Fogo"?',
+    'Cedric Diggory'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da cobra de Voldemort?',
+    'Nagini'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem são os pais de Draco Malfoy?',
+    'Lucius e Narcisa Malfoy'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual feitiço é usado para criar a "Marca Negra"?',
+    'Morsmordre'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o novo Ministro da Magia no último livro?',
+    'Rufus Scrimgeour'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o verdadeiro dono da Varinha das Varinhas?',
+    'Draco Malfoy, depois Harry Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que acontece com a varinha de Voldemort no final da série?',
+    'Ela é destruída por Harry Potter.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é o "Feitiço do Imperius"?',
+    'Um feitiço de controle mental que obriga a pessoa a obedecer a quem o lançou.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o verdadeiro mestre da Varinha das Varinhas?',
+    'Harry Potter, após derrotar Draco Malfoy.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é a professora de Herbologia?',
+    'Professor Pomona Sprout'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual o nome do livro que revela a verdade sobre a origem de Voldemort?',
+    'Os Contos de Beedle, o Bardo'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o Ministro da Magia no último livro?',
+    'Rufus Scrimgeour'
+);
+
+criaCartao(
+    'Harry Potter',
+    'O que é o "Cálice de Fogo"?',
+    'Um objeto mágico usado para selecionar os campeões do Torneio Tribruxo.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Como Harry Potter derrota Voldemort no último livro?',
+    'Destruindo as Horcruxes e depois derrotando-o na batalha final.'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem fica com a espada de Godric Gryffindor depois de Harry?',
+    'Hermione Granger'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome da esposa de Ron Weasley?',
+    'Hermione Granger'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do filho de Harry Potter?',
+    'James Sirius Potter'
+);
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o inimigo de Harry Potter em "Harry Potter e a Câmara Secreta"?',
+    'Tom Riddle'
+);
